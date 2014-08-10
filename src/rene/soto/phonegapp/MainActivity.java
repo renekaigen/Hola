053e08730916e79efe34007b9821475c4ce230e5
renekaigen <rene_rsl@hotmail.com>
2014-08-10 16:13:01 -0500

    dsdsad
    asdsad

---
+++ src/rene/soto/phonegapp/MainActivity.java
@@ -0,0 +1,17 @@
+package rene.soto.phonegapp;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MainActivity extends  DroidGap  {
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+
+}
